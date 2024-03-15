53a14c1f3ecea58becad0269ea1c82792524a363
zinghay1 <134397363+zinghay1@users.noreply.github.com>
2024-03-15 20:57:19 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,8 @@
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
-
-import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
-  const graphQLClient = new GraphQLClient(endpoint);
-
-  const query = gql`
-    {
-      posts(first: 10, where: { orderby: { field: DATE, order: DESC } }) {
-        nodes {
-          id
-          title
-          excerpt
-          featuredImage {
-            node {
-              sourceUrl
-              altText
-            }
-          }
-          categories {
-            nodes {
-              name
-            }
-          }
-          date
-          link
-        }
-      }
-    }
-  `;
-
-  const data = await graphQLClient.request(query);
-  const posts = data.posts.nodes;
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-interface Post {
-  id: string;
-  title: string;
-  excerpt: string;
-  featuredImage: {
-    node: {
-      sourceUrl: string;
-      altText: string;
-    };
-  };
-  categories: {
-    nodes: {
-      name: string;
-    }[];
-  };
-  date: string;
-  link: string;
-}
-
-
-
-
+import styles from '../styles/Home.module.css';
 
 interface Post {
   id: string;
@@ -128,6 +65,46 @@ const Home: NextPage<HomeProps> = ({ posts }) => {
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+  const graphQLClient = new GraphQLClient(endpoint);
+
+  const query = gql`
+    {
+      posts(first: 20, where: { orderby: { field: MODIFIED, order: DESC } }) {
+        nodes {
+          id
+          title
+          excerpt
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+          categories {
+            nodes {
+              name
+            }
+          }
+          modifiedGmt
+        }
+      }
+    }
+  `;
+
+  const data = await graphQLClient.request(query);
+  const posts: Post[] = data.posts.nodes;
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default Home;
 
 
 export default Home;
