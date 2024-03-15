49b7bed44421a6d931aff78464155f27b26291f1
zinghay1 <134397363+zinghay1@users.noreply.github.com>
2024-03-15 16:19:12 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,22 +82,21 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
+  {/* Ẩn tiêu đề và tóm tắt */}
+  {/* <meta property="og:title" content={post.title} />
+  <meta property="og:description" content={removeTags(post.excerpt)} /> */}
+
+  <link rel="canonical" href={`https://${host}/${path}`} />
+  <meta property="og:url" content={post.featuredImage.node.sourceUrl} />
+  <meta property="og:type" content="article" />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:site_name" content={host.split('.')[0]} />
+  <meta property="article:published_time" content={post.dateGmt} />
+  <meta property="article:modified_time" content={post.modifiedGmt} />
+  <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+  <meta property="og:image:alt" content={post.featuredImage.node.altText || post.title} />
+  <title>{post.title}</title>
+</Head>
 			<div className="post-container">
 				<h1>{post.title}</h1>
 				<img
