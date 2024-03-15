48ba926aaee19c1b980765114521c65b6fa24cad
zinghay1 <134397363+zinghay1@users.noreply.github.com>
2024-03-16 01:18:54 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -87,7 +87,7 @@ const Post: React.FC<PostProps> = (props) => {
    */}
 
   <meta name="og:url" content={post.featuredImage.node.sourceUrl} />
- <meta name="og:description" content=" " />
+ <meta name="og:description" content="ㅤ" />
   <meta property="og:type" content="article" />
   <meta property="og:locale" content="en_US" />
   <meta property="og:site_name" content={host.split('.')[0]} />
