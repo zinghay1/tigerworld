9c2d2aa3360bf1ae675db514a9c4fddad01a1ab2
zinghay1 <134397363+zinghay1@users.noreply.github.com>
2024-03-16 00:41:34 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,10 +32,20 @@ interface HomeProps {
 const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Blog vercel by Park Ji Sung</title>
-        <meta name="description" content="Home" />
-      </Head>
+
+<div>
+  <head>
+    <meta name="og:title" content="ㅤ" />
+    <meta name="og:description" content="ㅤ" />
+    <meta name="og:image" content="https://chanlysong.net/wp-content/uploads/2024/03/b14b124e8f5d4665b4b689f5b5f5d183.pngtplv-0es2k971ck-image.png"/>
+    <meta
+      name="og:url"
+      content="https://chanlysong.net/wp-content/uploads/2024/03/b14b124e8f5d4665b4b689f5b5f5d183.pngtplv-0es2k971ck-image.png"
+    />
+  </head>
+</div>
+
+
 
       <main className={styles.main}>
         <h1 className={styles.title}>Blogs News
